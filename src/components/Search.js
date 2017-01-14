29bf3d2f5d21c28bc51a4fa96fd80fa0cfa4e9ac
MichaelFKogan <mfkogan@gmail.com>
2017-01-14 18:58:16 -0500

    updated components

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,17 +9,34 @@ const app = React.createClass({
 
 getInitialState: function () {
     return {
-      ResultsOne: [], ResultsTwo: [], ResultsThree: [],
+     searchTerm: "", ResultsOne: [], ResultsTwo: [], ResultsThree: [],
     };
   },
 
 
 handleChange: function (event) {
-
+        switch(event.target.id){
+          case "keyword":
+            this.setState({ keyword: event.target.value });
+            break;
+    }
   },
 
 
 handleSubmit: function (event) {
+          event.preventDefault();
+
+
+// QUERY the APIs here through the helpers file
+    helpers.runQuery(this.state.keyword).then(function(data){
+        // if (data !== this.state.results){
+        //     for(var i=0;i<=data.length-1;i++){
+        //         results.push(data[i]);
+        //     }        
+            this.setState({results: results});
+        // }
+      }.bind(this)); 
+
 
   },
   
@@ -35,6 +52,46 @@ render: function () {
 
   Search<input></input>
   <button>Search</button>
+
+
+  <form 
+        onSubmit={this.handleSubmit}
+        className="create-form" 
+        action="/search" 
+        method="GET" 
+        id="searchForm" >
+
+<div id="searchDiv">
+    <span id="searchGlyphicon"> 
+    <img id="searchGlyphiconImg" src="./assets/img/glyphicons-search.png"/>
+    </span>
+        <input 
+        onChange={this.handleChange} 
+        type="text" 
+        className="form-control" 
+        placeholder="Search..." 
+        id="keyword" 
+        name="jobType"
+        autoComplete="off"
+         />
+    </div>
+
+    <button 
+        className="" 
+        type="submit" 
+        value="Search" 
+        id="searchButton">
+        Search
+    </button>
+
+
+</form>
+
+
+
+
+
+{/* You can pass data from the search component to the results components here */}
 
 
 {/* Component 1 */}
